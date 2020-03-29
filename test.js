45116b84185feb092aac80fa9c0e54d7a506617c
keroro-o <obata19760205akinobu@outlook.jp>
2020-03-30 04:06:09 +0900

    doneとdonelistのテストを実装

--- test.js
+++ test.js
@@ -8,5 +8,10 @@ todo.todo('ノートを買う');
 todo.todo('鉛筆を買う');
 assert.deepEqual(todo.list(), ['ノートを買う', '鉛筆を買う']);
 
+// done と donelist のテスト
+todo.done('鉛筆を買う');
+assert.deepEqual(todo.list(), ['ノートを買う']);
+assert.deepEqual(todo.donelist(), ['鉛筆を買う']);
+
 console.log('テストが正常に完了しました');
 
