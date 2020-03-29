3fd076d5c0a523352a69fe2e6c11a986fa5ad43a
keroro-o <obata19760205akinobu@outlook.jp>
2020-03-30 03:52:16 +0900

    todoとlistのテストを実装

---
+++ test.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const todo = require('./index.js');
+const assert = require('assert');   // Node.jsでテストをするためのモジュールのassertを呼び出して変数に格納
+
+// todo と list のテスト
+todo.todo('ノートを買う');
+todo.todo('鉛筆を買う');
+assert.deepEqual(todo.list(), ['ノートを買う', '鉛筆を買う']);
+
+console.log('テストが正常に完了しました');
+
